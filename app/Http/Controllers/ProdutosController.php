d0c853ff4a86e69e12640f53f610cde55ad0df96
AndersonTressoldi7 <andersontressoldi7@gmail.com>
2024-07-16 22:10:59 -0300

    adicionado tratamento de erros para os try catch

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -10,12 +10,22 @@ class ProdutosController extends Controller
     
     public function index()
     {
+        try{
         $produtos = Produto::all(); 
+        } catch(\Exception $e){
+            throw(['error' => 'Erro ao listar produtos!']);
+        }
         return response()->json($produtos, 200, [], JSON_UNESCAPED_UNICODE);
     }
 
-    public function indexProdutoEspecifico($id){
-        $produto = Produto::where('id', $id)->first();
+    public function indexProdutoEspecifico($id)
+    {
+        try {
+            $produto = Produto::where('id', $id)->first();
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Erro ao buscar produto pelo id!'], 500);
+        }
+    
         return response()->json($produto, 200, [], JSON_UNESCAPED_UNICODE);
     }
-}
+}    
